cdcc3b7f8aefd28385077503158a165a9518265e
Shane Vitarana <shanev@gmail.com>
2015-07-05 14:08:11 -0400

    added navbar from MStone

--- public/js/main.js
+++ public/js/main.js
@@ -6,3 +6,32 @@ $('body').vegas({
   ],
   overlay: true
 });
+
+/* ---------------------------------------------- /*
+ * Navbar
+/* ---------------------------------------------- */
+
+var navbar = $('.navbar');
+var navHeight = navbar.height();
+
+$(window).scroll(function() {
+  if($(this).scrollTop() >= navHeight) {
+    navbar.addClass('navbar-color');
+  }
+  else {
+    navbar.removeClass('navbar-color');
+  }
+});
+
+if($(window).width() <= 767) {
+  navbar.addClass('custom-collapse');
+}
+
+$(window).resize(function() {
+  if($(this).width() <= 767) {
+    navbar.addClass('custom-collapse');
+  }
+  else {
+    navbar.removeClass('custom-collapse');
+  }
+});
