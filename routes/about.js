eb718f00abab091fec8dc54e7fd4cdaa0d42b704
Shane Vitarana <shanev@gmail.com>
2015-07-05 22:42:14 -0400

    page css

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-/* GET home page. */
 router.get('/', function(req, res) {
   var file = path.join(__dirname, '../public', 'about.html');
   res.sendfile(file);
